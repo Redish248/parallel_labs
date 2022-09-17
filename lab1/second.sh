2a71d5367fce6f918b01d055bd1c516b88998878
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-09-17 19:20:27 +0300

    delete commected code

--- lab1/second.sh
+++ lab1/second.sh
@@ -21,40 +21,4 @@ for program_name in ${lab_names[@]}; do
     ((counter += delta))
   done
   ./program_name $n2 >>part2_seq.csv
-
 done
-#echo
-#echo "----------------------lab-par1------------------------------"
-#echo "Enter n1"
-#read n1
-#echo "Enter n2"
-#read n2
-#
-#((delta = ($n2 - $n1) / 10))
-#echo $delta
-#
-#counter=$n1
-#echo "n; ms"> lab1-par-1.csv
-#while [ $counter -lt $n2 ]; do
-#  ./lab1-par-1 $counter >> lab1-par-1.csv
-#  ((counter+=delta))
-#done
-#./lab1-par-1 $n2 >> lab1-par-1.csv
-#
-#echo
-#echo "----------------------lab-par3------------------------------"
-#echo "Enter n1"
-#read n1
-#echo "Enter n2"
-#read n2
-#
-#((delta = ($n2 - $n1) / 10))
-#echo $delta
-#
-#counter=$n1
-#echo "n; ms"> part2_par3.csv
-#while [ $counter -lt $n2 ]; do
-#  ./lab1-par-3 $counter >> part2_par3.csv
-#  ((counter+=delta))
-#done
-#./lab1-par-1 $n2 >> part2_par3.csv
