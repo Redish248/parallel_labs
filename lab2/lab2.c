138e2c7b3a7fb293ed08829e2d4a1566b913dd48
Irina Redkina <irisha132132@mail.ru>
2022-09-24 12:04:55 +0300

    lab2 change code

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -3,29 +3,10 @@
 #include <sys/time.h>
 #include <math.h>
 #include <time.h>
+#include <fwBase.h>
+#include <fwSignal.h>
 
 const int A = 936;
-
-void map_m1(double data[], int size) {
-    for (int i = 0; i < size; i++) {
-        data[i] = cosh(data[i]) + 1;
-    }
-}
-
-void map_m2(double data[], int size) {
-    double previous = 0;
-    for (int i = 0; i < size; i++) {
-        double tmp = data[i];
-        data[i] = fabs((double) 1 / tan(data[i] + previous));
-        previous = tmp;
-    }
-}
-
-void merge(double m1[], double m2[], int size) {
-    for (int i = 0; i < size; i++) {
-        m2[i] = (double) m1[i] / m2[i];
-    }
-}
 
 /* comb_sort: function to find the new gap between the elements */
 void comb_sort(double data[], int size) { //
@@ -64,19 +45,20 @@ double reduce(double data[], int size) {
 }
 
 int main(int argc, char *argv[]) {
-    int N;
+    int N, M;
     struct timeval T1, T2;
     long delta_ms;
 
-    if (argc < 2) {
-        printf("Need to add size of array as input argument\n");
+    if (argc < 3) {
+        printf("Need to add size of array and number of threads as input argument\n");
         return -1;
     }
 
     N = atoi(argv[1]); // N равен первому параметру командной строки
-    gettimeofday(&T1, NULL); // запомнить текущее время T1
+    M = atoi(argv[2]);
 
-    unsigned int seed = time(NULL);
+    fwSetNumThreads(M);
+    gettimeofday(&T1, NULL); // запомнить текущее время T1
 
     // 100 экспериментов
     for (int i = 0; i < 100; i++) {
@@ -84,28 +66,41 @@ int main(int argc, char *argv[]) {
 
         //Заполнить массив исходных данных размером N
         double *m1 = (double *) malloc(N * sizeof(double));
-        for (int j = 0; j < N; j++) {
-            double value = 1 + rand_r(&seed) % (A - 1);
+        for (unsigned int j = 0; j < N; j++) {
+            double value = 1 + rand_r(&j) % (A - 1);
             // double value = 1 + rand() % (A - 1);
             m1[j] = value;
         }
 
         double *m2 = (double *) malloc(N / 2 * sizeof(double));
-        for (int j = 0; j < N / 2; j++) {
-            double value = A + rand_r(&seed) % (A*10 - A);
+        double *m2_copy = (double *) malloc(N / 2 * sizeof(double));
+        for (unsigned int j = 0; j < N / 2; j++) {
+            double value = A + rand_r(&j) % (A*10 - A);
             //double value = A + rand() % (A * 10 - A);
             m2[j] = value;
         }
 
         // Решить поставленную задачу, заполнить массив с результатами
 
-        //MAP: var 2 - гиперболический косинус с последующим увеличением на 1
-        map_m1(m1, N);
-        // var 4 - модуль котангенса
-        map_m2(m2, N / 2);
+        //MAP: M1 -- var 2 - гиперболический косинус с последующим увеличением на 1
+        fwsCosh_64f_A50(m1, m1, N);
+        fwsAddC_64f(m1, 1, m1, N);
+
+        // M2 -- var 4 - модуль котангенса(элемент + предыдущий)
+
+        //сначала копируем массив
+        fwsCopy_64f(m2, m2_copy+1, N/2);
+        m2_copy[0] = 0;
+
+        //теперь abs(ctg(data+prev))
+        fwsAdd_64f_I(m2_copy, m2, N/2);
+        fwsTan_64f_A50(m2, m2, N/2);
+        //FIXME: функция под x32!!!
+        fwsDivCRev_32f_I(1, (Fw32f *) m2, N / 2);
+        fwsAbs_64f(m2, m2, N/2);
 
         //Merge: var 2 - деление M2[i] = M[i]/M2[i]
-        merge(m1, m2, N / 2);
+        fwsDiv_64f(m1, m2,  m2, N / 2);
 
         //Sort: var 2 - сортировка расческой
         comb_sort(m2, N / 2);
