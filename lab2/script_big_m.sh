07ecc26d290a24539534faf2905f74244c124666
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-09-25 00:16:02 +0300

    add script for add_task

--- lab2/script_big_m.sh
+++ lab2/script_big_m.sh
@@ -4,7 +4,7 @@ program_name="lab2"
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lib/lib
 make clean
 make
-for i in 0 1 2 3; do
+for i in 0; do
   echo "-------------------"
   n2=${n2_arr[$i]}
   result_file_name=${program_name}_big_m_$i.csv
