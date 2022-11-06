cc157ce43aa6a12c6434a9c9100fa8f5af1dc816
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-11-06 18:34:41 +0300

    dynamic - try 1

--- lab5/lab5_dynamic.c
+++ lab5/lab5_dynamic.c
@@ -8,9 +8,9 @@
 #include <pthread.h>
 
 const int A = 936;
-pthread_mutex_t print_mutex;
+pthread_mutex_t print_mutex, chunk_mutex;
 double *m1, *m2, *m2_copy;
-int N, FOR_I, THREAD_NUM;
+int N, FOR_I, THREAD_NUM, start_chunk_size;
 
 //Барьер - глобальная переменная
 static pthread_barrier_t barrier;
@@ -20,6 +20,46 @@ struct main_args {
     int id;
 
 };
+
+int cosh_index = -1;
+int fabs_index = -1;
+int merge_index = -1;
+
+int get_cosh_index() {
+    int r;
+    pthread_mutex_lock(&chunk_mutex);
+    r = cosh_index + 1;
+    cosh_index += start_chunk_size;
+    if (r >= N) {
+        r = -1; // stop
+    }
+    pthread_mutex_unlock(&chunk_mutex);
+    return r;
+}
+
+int get_fabs_index() {
+    int r;
+    pthread_mutex_lock(&chunk_mutex);
+    r = fabs_index + 1;
+    fabs_index += start_chunk_size;
+    if (r >= N) {
+        r = -1; // stop
+    }
+    pthread_mutex_unlock(&chunk_mutex);
+    return r;
+}
+
+int get_merge_index() {
+    int r;
+    pthread_mutex_lock(&chunk_mutex);
+    r = merge_index + 1;
+    merge_index += start_chunk_size;
+    if (r >= N) {
+        r = -1; // stop
+    }
+    pthread_mutex_unlock(&chunk_mutex);
+    return r;
+}
 
 /* comb_sort: function to find the new gap between the elements */
 void comb_sort() { //
@@ -38,33 +78,56 @@ void comb_sort() { //
     }
 }
 
-void cosh_part(int start_i, int len) {
-    int counter = 0;
-    while (counter < len) {
-        m1[start_i + counter] = cosh(m1[start_i + counter]) + 1;
-        ++counter;
-    }
-}
+void cosh_part(int max_size) {
+    int start_i = get_cosh_index();
 
-void fabs_part(int start_i, int len) {
-    int counter = 0;
-    while (counter < len) {
-        int k = start_i + counter;
-        if (k == 0) {
-            m2[k] = fabs((double) 1 / tan(m2[k]));
-        } else {
-            m2[k] = fabs((double) 1 / tan(m2[k] + m2_copy[k - 1]));
+    while (start_i >= 0) {
+        int len = start_chunk_size;
+        if (start_i + len > max_size) len = max_size - start_i;
+
+        int counter = 0;
+        while (counter < len) {
+            m1[start_i + counter] = cosh(m1[start_i + counter]) + 1;
+            ++counter;
         }
-        ++counter;
+        start_i = get_cosh_index();
     }
 }
 
-void merge_part(int start_i, int len) {
-    int counter = 0;
-    while (counter < len) {
-        int k = start_i + counter;
-        m2[k] = (double) m1[k] / m2[k];
-        ++counter;
+void fabs_part(int max_size) {
+    int start_i = get_fabs_index();
+
+    while (start_i >= 0) {
+        int len = start_chunk_size;
+        if (start_i + len > max_size) len = max_size - start_i;
+        int counter = 0;
+        while (counter < len) {
+            int k = start_i + counter;
+            if (k == 0) {
+                m2[k] = fabs((double) 1 / tan(m2[k]));
+            } else {
+                m2[k] = fabs((double) 1 / tan(m2[k] + m2_copy[k - 1]));
+            }
+            ++counter;
+        }
+        start_i = get_fabs_index();
+    }
+
+}
+
+void merge_part(int max_size) {
+    int start_i = get_merge_index();
+
+    while (start_i >= 0) {
+        int len = start_chunk_size;
+        if (start_i + len > max_size) len = max_size - start_i;
+        int counter = 0;
+        while (counter < len) {
+            int k = start_i + counter;
+            m2[k] = (double) m1[k] / m2[k];
+            ++counter;
+        }
+        start_i = get_merge_index();
     }
 }
 
@@ -74,18 +137,6 @@ void *main_function(void *args) {
     int id = thread_args.id;
     unsigned int tmp1 = thread_args.index;
     unsigned int tmp2 = thread_args.index;
-
-    int chunk_size_1 = N / THREAD_NUM;
-    int start_i_1 = id * chunk_size_1;
-    if (id == THREAD_NUM - 1) { // last
-        chunk_size_1 = N - start_i_1;
-    }
-
-    int chunk_size_2 = N / THREAD_NUM / 2;
-    int start_i_2 = id * chunk_size_2;
-    if (id == THREAD_NUM - 1) { // last
-        chunk_size_2 = N - start_i_2;
-    }
 
     /*
     pthread_mutex_lock(&print_mutex);
@@ -123,8 +174,10 @@ void *main_function(void *args) {
     pthread_barrier_wait(&barrier); // join потоков
 
     // MAP
-    if (chunk_size_1 > 0) cosh_part(start_i_1, chunk_size_1);
-    if (chunk_size_2 > 0) fabs_part(start_i_2, chunk_size_2);
+    cosh_part(N);
+    fabs_part(N / 2);
+
+    pthread_barrier_wait(&barrier); // join потоков
 
     /*
     pthread_mutex_lock(&print_mutex);
@@ -141,10 +194,11 @@ void *main_function(void *args) {
     pthread_mutex_unlock(&print_mutex);
     */
 
-    pthread_barrier_wait(&barrier); // join потоков
 
     // MERGE
-    if (chunk_size_2 > 0) merge_part(start_i_2, chunk_size_2);
+    merge_part(N / 2);
+
+    pthread_barrier_wait(&barrier); // join потоков
 
     /*
     pthread_mutex_lock(&print_mutex);
@@ -157,8 +211,6 @@ void *main_function(void *args) {
     printf("thread %d merge arr\n", id);
     pthread_mutex_unlock(&print_mutex);
    */
-
-    pthread_barrier_wait(&barrier); // join потоков
 
     // SORT
     if (id == 0) {
@@ -175,11 +227,11 @@ void *main_function(void *args) {
             }
         }
 
-        /*
+//        /*
         pthread_mutex_lock(&print_mutex);
         printf("X: %f\n", result);
         pthread_mutex_unlock(&print_mutex);
-         */
+//         */
     }
 
     /*
@@ -196,16 +248,26 @@ int main(int argc, char *argv[]) {
     struct timeval T1, T2;
     long delta_ms;
 
-    if (argc < 3) {
+    if (argc < 4) {
         printf("Need to add size of array and number of threads as input arguments\n");
         return -1;
     }
 
     N = atoi(argv[1]);
     int M = atoi(argv[2]);
-    if (M < 1) printf("threads_counter must be positive");
-    if (argc >= 4) {
-        FOR_I = atoi(argv[3]);
+    if (M < 1) {
+        printf("threads_counter must be positive");
+        return -1;
+    }
+
+    start_chunk_size = atoi(argv[3]);
+    if (start_chunk_size < 1 || start_chunk_size >= N) {
+        printf("incorrect start_chuck_size parameter");
+        return -2;
+    }
+
+    if (argc >= 5) {
+        FOR_I = atoi(argv[4]);
     } else FOR_I = 100;
 
     THREAD_NUM = M;
@@ -216,6 +278,7 @@ int main(int argc, char *argv[]) {
     m2_copy = (double *) malloc(N / 2 * sizeof(double));
 
     pthread_mutex_init(&print_mutex, NULL);
+    pthread_mutex_init(&chunk_mutex, NULL);
     pthread_barrier_init(&barrier, NULL, THREAD_NUM); //инициализация барьера
     pthread_t thread[THREAD_NUM];
     struct main_args thread_args[THREAD_NUM];
@@ -236,6 +299,9 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < THREAD_NUM; i++) {
             pthread_join(thread[i], NULL);
         }
+        cosh_index = -1;
+        fabs_index = -1;
+        merge_index = 1;
     }
 
     //Выход из потока:
