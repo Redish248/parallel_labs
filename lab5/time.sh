68042a3e06625f92650a86dda922424993822306
AnastasiyaSmirnova <ansmir10.99@gmail.com>
2022-11-06 21:03:32 +0300

    time with N parameter

--- lab5/time.sh
+++ lab5/time.sh
@@ -12,12 +12,11 @@ for program_name in "${programs[@]}"; do
   full_result_file+=$result_file
   full_result_file+=$program_name
   full_result_file+=_$m
-  full_result_file+='.csv'
+  full_result_file+=_
   echo $program_name
-  echo "generate; map; merge; sort" >$full_result_file
   while [ $i -lt $n2 ]; do
     echo $i
-    ./$program_name $i $m >> $full_result_file
+    ./$program_name $i $m >> $full_result_file$i.csv
     ((i = $i + 62775))
   done
   full_result_file=''
