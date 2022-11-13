e1fe03f7a14eb69bf167719f779d6c7c10a955b8
Irina Redkina <irisha132132@mail.ru>
2022-11-13 11:33:15 +0300

    lab6: add generate

--- lab6_cuda/main.cu
+++ lab6_cuda/main.cu
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
-#include <time.h>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -42,47 +42,40 @@ int main(int argc, char *argv[]) {
     cudaMalloc(&m2v, sizeof(double) * N / 2);
     cudaMalloc(&m2_copyv, sizeof(double) * N / 2);
 
-    //gettimeofday(&T1, NULL)
+    gettimeofday(&T1, nullptr);
 
-    // 100 экспериментов
-   /* for (unsigned int i = 0; i < K; i++) {
-        //GENERATE:
-        unsigned int tmp1 = i;
-        unsigned int tmp2 = i;
-        //Заполнить массив исходных данных размером N
-        for (int j = 0; j < N; j++) {
-            double value = 1 + rand_r(&tmp1) % (A - 1);
-            m1[j] = value;
-        }
+    // TODO: 100 экспериментов - вернуть цикл
+    //GENERATE:
+    unsigned int tmp1 = 10; //TODO: tmp1 = i;
+    unsigned int tmp2 = 10; // tODO: tmp2 = i;
+    //Заполнить массив исходных данных размером N
+    for (int j = 0; j < N; j++) {
+        double value = 1 + rand_r(&tmp1) % (A - 1);
+        m1[j] = value;
+    }
 
-        for (int j = 0; j < N / 2; j++) {
-            double value = A + rand_r(&tmp2) % (A * 10 - A);
-            m2[j] = value;
-            m2_copy[j] = value;
-        }
+    for (int j = 0; j < N / 2; j++) {
+        double value = A + rand_r(&tmp2) % (A * 10 - A);
+        m2[j] = value;
+       // m2_copy[j] = value;
+    }
 
+    for (int i = 0; i < N; i++) {
+        cout << "m1 " << m1[i] << "\n";
+    }
 
-    }*/
-
-   m1[0] = 1;
-   m1[1] = 2;
-   m1[2] = 3;
-    m1[3] = 4;
-    m1[4] = 5;
-    m1[5] = 6;
-    m2[0] = 1;
-    m2[1] = 2;
-    m2[2] = 3;
+    for (int i = 0; i < N / 2; i++) {
+        cout << "m2 " << m2[i] << "\n";
+    }
 
     cudaMemcpy(m1v, m1, sizeof(double) * N, cudaMemcpyHostToDevice);
     cudaMemcpy(m2v, m2, sizeof(double) * N / 2, cudaMemcpyHostToDevice);
     cudaMemcpy(m2_copyv, m2_copy, sizeof(double) * N / 2, cudaMemcpyHostToDevice);
 
-    dim3 gridSize = dim3(1, 1, 1);    //Размер используемого грида
-    dim3 blockSize = dim3(N / 2, 1, 1); //Размер используемого блока
+    dim3 gridSize = dim3(1, 1, 1);    //TODO: Размер используемого грида
+    dim3 blockSize = dim3(N / 2, 1, 1); //TODO: Размер используемого блока
 
-
-    sum<<<gridSize, blockSize>>>(m1v, m2v, m2_copyv, N / 2 );
+    sum<<<gridSize, blockSize>>>(m1v, m2v, m2_copyv, N / 2);
 
     //Хендл event'а
     cudaEvent_t syncEvent;
@@ -99,14 +92,17 @@ int main(int argc, char *argv[]) {
 
     cudaEventDestroy(syncEvent);
 
-
     cudaFree(m1);
     cudaFree(m2);
     cudaFree(m2_copy);
 
-    //gettimeofday(&T2, NULL); // запомнить текущее время T2
-    //delta_ms = (T2 - T1) * 1000;
-//    printf("\nN=%d. Milliseconds passed: %ld\n", N, delta_ms); /* T2 - T1 */
-   // printf("%d;%ld\n", N, delta_ms); /* T2 - T1 */
+    free(m1);
+    free(m2);
+    free(m2_copy);
+
+    gettimeofday(&T2, nullptr); // запомнить текущее время T2
+    delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000;
+   // printf("\nN=%d. Milliseconds passed: %ld\n", N, delta_ms); /* T2 - T1 */
+    printf("%d;%ld\n", N, delta_ms); /* T2 - T1 */
     return 0;
 }
